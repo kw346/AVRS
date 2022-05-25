fcaaa6442af27b7cd092c1a7c1b214428a2509a9
zanewashere <20045805@myrp.edu.sg>
2022-05-25 15:53:45 +0800

    Added id into TextInput for username and password added login

--- app/src/main/java/sg/edu/rp/c346/id20022735/avrs/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022735/avrs/MainActivity.java
@@ -6,22 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin;
+    TextInputEditText textUsername;
+    TextInputEditText textPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         btnLogin = findViewById(R.id.login);
+        textUsername = findViewById(R.id.textuser);
+        textPassword = findViewById(R.id.textpass);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomePage.class);
-                startActivity(intent);
+                final String username, password;
+                username = String.valueOf(textUsername.getText().toString());
+                password = String.valueOf(textPassword.getText().toString());
+                if (!username.equals("") && !password.equals("")) {
+                    if (username.equals("user") && password.equals("test")) {
+                        Intent intent = new Intent(MainActivity.this, HomePage.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),"Invalid account",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"All fields required",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
