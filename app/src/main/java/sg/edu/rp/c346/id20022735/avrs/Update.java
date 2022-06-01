1f8031d4533e821b48661f7674d4b5d9b94822e5
EugeneCW <82862128+ChiaWaiEugene@users.noreply.github.com>
2022-06-01 14:48:32 +0800

    Update.xml

---
+++ app/src/main/java/sg/edu/rp/c346/id20022735/avrs/Update.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20022735.avrs;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
