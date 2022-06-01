f89747b87e3348b3da90b19dee5a3a072e4f28e2
kw346 <20022735@myrp.edu.sg>
2022-06-01 17:25:19 +0800

    Change layout for Add/Admin pg;Codes for add

--- app/src/main/java/sg/edu/rp/c346/id20022735/avrs/AdminPage.java
+++ app/src/main/java/sg/edu/rp/c346/id20022735/avrs/AdminPage.java
@@ -2,13 +2,27 @@ package sg.edu.rp.c346.id20022735.avrs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminPage extends AppCompatActivity {
 
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
+        btn1 = findViewById(R.id.add);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminPage.this, Add.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/sg/edu/rp/c346/id20022735/avrs/Update.java
+++ app/src/main/java/sg/edu/rp/c346/id20022735/avrs/Update.java
@@ -1,10 +1,14 @@
 package sg.edu.rp.c346.id20022735.avrs;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +21,9 @@ import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
 public class Update extends AppCompatActivity {
+    private static final String url = "jdbc:mysql://116.89.52.49:3306/fyp";
+    private static final String user = "root";
+    private static final String pass = "123";
 
     TextView tvStaffId;
     EditText etName, etLicense1, etLicense2, etDesignation, etContact;
@@ -31,7 +38,6 @@ public class Update extends AppCompatActivity {
         tvStaffId = findViewById(R.id.staff_id);
         etName = findViewById(R.id.name);
         etLicense1 = findViewById(R.id.license1);
-        etLicense2 = findViewById(R.id.license2);
         etDesignation = findViewById(R.id.designation);
         etContact = findViewById(R.id.contact);
         rgSchool = findViewById(R.id.school);
@@ -43,29 +49,57 @@ public class Update extends AppCompatActivity {
         if (rgSchool.getCheckedRadioButtonId() == R.id.soi) {
 
         }
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //grab string from TextInputEditText
+//                String username, password;
+//                username = String.valueOf(textUsername.getText().toString());
+//                password = String.valueOf(textPassword.getText().toString());
+//                //Check if all fields are entered
+//                if (!username.equals("") && !password.equals("")) {
+//                    //execute sql code (see below at ConnectMySql private class)
+//                    MainActivity.ConnectMySql connectMySql = new MainActivity.ConnectMySql();
+//                    connectMySql.execute("");
+//                }
+//                //run if one of the fields are empty
+//                else {
+//                    Toast.makeText(getApplicationContext(),"All fields required",Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
 
-        protected String doInBackground(String... params) {
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection con = DriverManager.getConnection(staff_id, name, license1, license2, designation, contact, school);
-                System.out.println("Database connection success");
-                //set sql string
-                String sql = "UPDATE (table name) SET name = etName, license1 = etLicense1, license2 = etLicense2, designation = etDesignation, contact= etContact, school = rgSchool " +
-                        "WHERE staff_id = tvStaffId ";
-                String result = "Database Connection Successful\n";
-
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery(sql);
-                ResultSetMetaData rsmd = rs.getMetaData();
-                while (rs.next()) {
-                    result = rs.getString(1).toString();
-                }
-                res = result;
-            } catch (Exception e) {
-                e.printStackTrace();
-                res = e.toString();
-            }
-            return res;
-        }
     }
+
+//    private class ConnectMySql extends AsyncTask<String, Void, String> {
+//        //define variables
+//        String res = "";
+//        String username = String.valueOf(textUsername.getText().toString());
+//        String password = String.valueOf(textPassword.getText().toString());
+//
+//        //main part where it process
+//        protected String doInBackground(String... params) {
+//            try {
+//                Class.forName("com.mysql.jdbc.Driver");
+//                Connection con = DriverManager.getConnection(staff_id, name, license1, license2, designation, contact, school);
+//                System.out.println("Database connection success");
+//                //set sql string
+//                String sql = "UPDATE (table name) SET name = etName, license1 = etLicense1, license2 = etLicense2, designation = etDesignation, contact= etContact, school = rgSchool " +
+//                        "WHERE staff_id = tvStaffId ";
+//                String result = "Database Connection Successful\n";
+//
+//                Statement st = con.createStatement();
+//                ResultSet rs = st.executeQuery(sql);
+//                ResultSetMetaData rsmd = rs.getMetaData();
+//                while (rs.next()) {
+//                    result = rs.getString(1).toString();
+//                }
+//                res = result;
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                res = e.toString();
+//            }
+//            return res;
+//        }
+//    }
 }
